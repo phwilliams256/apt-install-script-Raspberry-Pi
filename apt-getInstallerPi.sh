d877bd50d7b5bef108e2db3e2a7eeabe684eb8a3
phwilliams256 <71341873+phwilliams256@users.noreply.github.com>
2022-01-07 16:07:04 -0600

    Add files via upload

---
+++ apt-getInstallerPi.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+apt-get update
+apt-get -y dist-upgrade
+apt-get -y autoremove
+
+# apt-cache search SearchTerm
+################################################################################
+exit
+
+apt-get -y install dos2unix
+apt-get -y install arp-scan
+apt-get -y install macchanger
+apt-get -y install lsof 
+apt-get -y install ipcalc
+
+apt-get -y install vim
+apt-get -y install tshark
+apt -y install bc
+apt -y install tcpdump
+apt-get -y install ntfs-3g
+apt-get -y install nmap
+apt-get -y install gnuplot
+apt-get -y install bsd-mailx
+aptitude -y install etherwake
+apt-get -y install expect
+apt-get -y install geoip-bin
+
+###############################################################################
+
